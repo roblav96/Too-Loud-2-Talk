e232e9dc43d5c8602cd566052f35c534c4d90be7
Rob <roblav96@gmx.com>
2012-03-22 03:38:29 -0400

    rstyj

--- js/too.loud.js
+++ js/too.loud.js
@@ -1,44 +1,51 @@
-var gWOrientation, gWH, gWW, gIntFont;
-$$(document).ready(function() {
+var gWOrientation, gWH, gWW;
+$(document).ready(function() {
 	onWindowResize();
 });
 
 function clickedButton() {
-	var objText = $$("#objText");
-	objText.html(document.getElementById("textInput").value);
-	var objTextSize = gIntFont;
-	objText.css("font-size", objTextSize);
-	console.log(objText.css("font-size"));
-	var textH, textW;
-	if (gWOrientation == "portrait") {
-		do {
-			textW = objText.width();
-			objTextSize = objTextSize - 1;
-			objText.css("font-size", objTextSize);
-		} while (textW > gWW);
-	} else {
-		do {
-			textH = objText.height();
-			objTextSize = objTextSize - 1;
-			objText.css("font-size", objTextSize);
-		} while (textH > gWH);
-	}
+	var objText = $("#objText");
+	objText.html(id$("textInput").value);
+	var textH = id$("objText").offsetHeight;
+	var textW = id$("objText").offsetWidth;
+	objText.css("font-size", gWH + "px");
+
+	do {
+		objText.css("font-size", (parseInt(objText.css("font-size"), 10) - 1) + "px");
+	} while ((id$("objText").offsetWidth > gWW));
+
+	do {
+		objText.css("font-size", (parseInt(objText.css("font-size"), 10) - 1) + "px");
+	} while ((id$("objText").offsetHeight > gWH));
+
+	//console.log(id$("objText").offsetHeight + " " + id$("objText").offsetWidth + " " + parseInt(objText.css("font-size"), 10) + " " + gWH);
+}
+
+function clickedTools() {
+	$("#jQUi").popup({
+		title: "TOOLS",
+		message: "hello!",
+		cancelText: "CLOSE",
+		cancelCallback: function() {},
+		cancelOnly: true
+	});
+}
+
+function id$(i) {
+	return document.getElementById(i);
 }
 
 function onWindowResize() {
 	gWH = window.innerHeight * 0.9;
 	gWW = window.innerWidth * 0.9;
-	gIntFont = gWH;
 	if (gWW > gWH) {
 		gWOrientation = "landscape";
 	} else {
 		gWOrientation = "portrait";
 	}
-	var btnW = $$("#textSubmit").outerWidth();
-	$$("#textInput").css("width", ((gWW * 1.1) - btnW - 25));
-	$$("#textInput").css("height", $$("#navbar").height() * 0.8);
-	$$("#textInput").css("font-size", $$("#navbar").height() * 0.25);
-	$$("#textSubmit").css("height", $$("#navbar").height() * 0.8);
+	var newWidth = id$("navbar").offsetWidth - id$("textSubmit").offsetWidth - id$("textTools").offsetWidth - 50;
+	console.log(id$("textSubmit").offsetWidth + " " + id$("textTools").offsetWidth + " " + id$("navbar").offsetWidth);
+	$("#textInput").css("width", newWidth + "px");
 }
 
 window.onresize = function() {
